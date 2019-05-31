12f343fbb3af2f5dc624120b7868c3493ab44fa5
Giovanni Marçon Rossi <giovanni.mar.rossi@gamil.com>
2019-05-31 20:08:32 -0300

    Todas as tranformações agora estão sendo feitas com matrizes

--- js/scenes/delorean.js
+++ js/scenes/delorean.js
@@ -5,7 +5,6 @@ let loader = new THREE.GLTFLoader();
 let delorean = loader.load(
     'models/low_poly_art_delorean/scene.gltf',
     function (gltf) {
-        gltf.scene.position.set(0, -6, 0);
         scene.add(gltf.scene);
     },
     function (xhr) {
