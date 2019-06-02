d646f4896e27748c730f0e604eb4a011a1d50c8c
Eiji Uema <eijiuema@gmail.com>
2019-06-02 19:16:10 -0300

    Merge fix

--- js/scenes/delorean.js
+++ js/scenes/delorean.js
@@ -2,13 +2,8 @@ let scene = new THREE.Scene();
 
 let loader = new THREE.GLTFLoader();
 
-<<<<<<< HEAD
 loader.load(
-    'models/low_poly_art_delorean/good_delorean.glb',
-=======
-let delorean = loader.load(
     'models/low_poly_art_delorean/delorean.glb',
->>>>>>> 3ca9f34bc740d0caece923754d7fc8d4605f1e69
     function (gltf) {
         scene.add(gltf.scene);
     },
